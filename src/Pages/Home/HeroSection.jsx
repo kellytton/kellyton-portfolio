6cb279d4eb10be2adff233aab5c8137a892cd4cf
Kelly Ton <kthton@gmail.com>
2025-03-11 20:42:19 -0400

    Start hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,5 +1,23 @@
 export default function HeroSection() {
     return (
-        <h1>HeroSection</h1>
+        <section id="heroSection" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section-content">
+                    <p className="section--title">Hey, I'm Kelly</p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section-title--color">Full Stack</span>{" "}
+                        <br />
+                        Developer
+                    </h1>
+                    <p className="hero--section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        <br/> A necessitatibus et quia minus illo!
+                    </p>
+                </div>
+                <button className="btn btn-primary">Get In Touch</button>
+            </div>
+            <div className="hero--section--img">
+                <img src="./img/hero_img.png" alt="Hero Section"/>
+            </div>
+        </section>
     );
 }
