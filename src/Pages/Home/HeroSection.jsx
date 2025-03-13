9c87579f5eed2b7830fcbd92d925943ac92fb6be
Kelly Ton <kthton@gmail.com>
2025-03-13 14:35:54 -0400

    Complete hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -2,7 +2,7 @@ export default function HeroSection() {
     return (
         <section id="heroSection" className="hero--section">
             <div className="hero--section--content--box">
-                <div className="hero--section-content">
+                <div className="hero--section--content">
                     <p className="section--title">Hey, I'm Kelly</p>
                     <h1 className="hero--section--title">
                         <span className="hero--section-title--color">Full Stack</span>{" "}
