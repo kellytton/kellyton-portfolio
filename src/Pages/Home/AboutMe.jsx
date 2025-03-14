56f42e53aa969a858315b8ce79fab0a4b3e75c9e
Kelly Ton <kthton@gmail.com>
2025-03-14 15:47:53 -0400

    Complete about me section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,25 @@
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="About Me" />
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">
+                        About
+                    </p>
+                    <h1 className="skills-section--heading">
+                        About Me
+                    </h1>
+                    <p className="hero--section-description">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam porro repellat totam odit exercitationem, ad ut nesciunt, laudantium sunt dicta dolores, itaque doloremque provident. Alias laudantium itaque illo qui quaerat.
+                    </p>
+                    <p className="hero--section-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ex sequi laboriosam doloribus illo eveniet!
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
